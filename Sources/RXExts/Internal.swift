e02b99e58e99ac7d890bb045e014113ed2c9d305
yaochenfeng <282696845@qq.com>
2022-11-10 10:02:33 +0800

    add additionalSafeAreaInsets

--- Sources/RXExts/Internal.swift
+++ Sources/RXExts/Internal.swift
@@ -2,6 +2,7 @@
 internal struct AssociatedKeys {
     static var dispose: UInt8 = 0
     static var layoutGuide: UInt8 = 0
+    static var safeAreaInsets: UInt8 = 0
 }
 
 internal let logger = Logger(rx: RXUIView.self)
--- Sources/RXExts/exts/UIViewController+rx.swift
+++ Sources/RXExts/exts/UIViewController+rx.swift
@@ -14,6 +14,31 @@ public extension Reactive where Base: UIViewController {
         guard let name = Bundle.main.infoDictionary?["UILaunchStoryboardName"] as? String else { return nil }
         return UIStoryboard(name: name, bundle: nil).instantiateInitialViewController()
     }
+    /// 附加安全边距
+    var additionalSafeAreaInsets: UIEdgeInsets {
+        set {
+            if #available(iOS 11.0, *) {
+                base.additionalSafeAreaInsets = newValue
+            } else {
+                objc_setAssociatedObject(base, &AssociatedKeys.safeAreaInsets, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                base.view.rx.safeAreaLayoutGuide.snp.remakeConstraints { make in
+                    make.top.equalTo(base.topLayoutGuide.snp.bottom).offset(newValue.top)
+                    make.left.equalTo(base.view).offset(newValue.left)
+                    make.right.equalTo(base.view).offset(-newValue.right)
+                    make.bottom.equalTo(base.bottomLayoutGuide.snp.top).offset(-newValue.bottom)
+                }
+            }
+        }
+        get {
+            if #available(iOS 11.0, *) {
+                return base.additionalSafeAreaInsets
+            }
+            if let obj = objc_getAssociatedObject(base, &AssociatedKeys.safeAreaInsets) as? UIEdgeInsets {
+                return obj
+            }
+            return .zero
+        }
+    }
 }
 
 #endif
